0dcc9c1c7cc135e534c6e56bdb88d58fab4cf03f
Sayel Munsi <sayelmunsi@gmail.com>
2023-07-15 22:50:36 +0530

    send

--- haldiaform/haldiaform/urls.py
+++ haldiaform/haldiaform/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('form.urls')),
+    path("",include('forms.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
