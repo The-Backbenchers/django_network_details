1023b40808ba9533ab0088b9e820de32deccb1cf
Sayel Munsi <sayelmunsi@gmail.com>
2023-07-15 23:50:09 +0530

    removed database

--- haldiaform/haldiaform/settings.py
+++ haldiaform/haldiaform/settings.py
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'haldiaform.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'), 
-        'USER': os.getenv('DB_USER'), 
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'), 
-        'PORT': os.getenv('DB_PORT'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.getenv('DB_NAME'), 
+    #     'USER': os.getenv('DB_USER'), 
+    #     'PASSWORD': os.getenv('DB_PASSWORD'),
+    #     'HOST': os.getenv('DB_HOST'), 
+    #     'PORT': os.getenv('DB_PORT'),
+    # }
 }
 
 
