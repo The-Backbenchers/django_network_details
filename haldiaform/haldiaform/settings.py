a4067048b7966d3c208964b88b98c67d96999b81
Sayel Munsi <sayelmunsi@gmail.com>
2023-07-15 21:17:00 +0530

    checking

--- haldiaform/haldiaform/settings.py
+++ haldiaform/haldiaform/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('HIDDEN_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
