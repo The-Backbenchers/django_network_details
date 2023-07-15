a497937336c12e032fd05e45bb4095c0e8bf8bd9
Sayel Munsi <sayelmunsi@gmail.com>
2023-07-16 02:27:41 +0530

    database added

--- haldiaform/haldiaform/settings.py
+++ haldiaform/haldiaform/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'haldiaform.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': os.getenv('DB_NAME'), 
-    #     'USER': os.getenv('DB_USER'), 
-    #     'PASSWORD': os.getenv('DB_PASSWORD'),
-    #     'HOST': os.getenv('DB_HOST'), 
-    #     'PORT': os.getenv('DB_PORT'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'), 
+        'USER': os.getenv('DB_USER'), 
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'), 
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 # DATABASES = {
 #     'default': {
