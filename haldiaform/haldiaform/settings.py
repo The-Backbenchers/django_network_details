d8a50767208f1fb9e3d350bd16e0462f1bbb5e36
Sayel Munsi <sayelmunsi@gmail.com>
2023-07-15 19:27:11 +0530

    bug fix1

--- haldiaform/haldiaform/settings.py
+++ haldiaform/haldiaform/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('HIDDEN_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
