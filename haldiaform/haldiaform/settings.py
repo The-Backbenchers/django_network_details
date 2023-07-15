571ef8252d4aee7c213bc6244fdfda8748fcdd6e
Sayel Munsi <sayelmunsi@gmail.com>
2023-07-15 15:48:22 +0530

    postgres database added, vercel json added, ready for deployment

--- haldiaform/haldiaform/settings.py
+++ haldiaform/haldiaform/settings.py
@@ -78,10 +78,20 @@ WSGI_APPLICATION = 'haldiaform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'), 
+        'USER': os.getenv('DB_USER'), 
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'), 
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
