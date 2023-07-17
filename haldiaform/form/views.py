3962a3552d041f3b9f38a4ed1c70df5e6c7e82a2
Sayel Munsi <sayelmunsi@gmail.com>
2023-07-17 10:17:53 +0530

    html files modified

--- haldiaform/form/views.py
+++ haldiaform/form/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
+def Home(request):
     # return HttpResponse("Hello World")
     return render(request,'base.html')
+def signup(request):
+    # return HttpResponse("Hello World")
+    return render(request,'signup.html')
