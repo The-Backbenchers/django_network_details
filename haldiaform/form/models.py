417ac4877b631e574f0c3634191abdc1f966270f
Sayel Munsi <sayelmunsi@gmail.com>
2023-07-19 02:00:41 +0530

    Model with relationships ready

--- haldiaform/form/models.py
+++ haldiaform/form/models.py
@@ -13,29 +13,35 @@ class owner_details(models.Model):
     owner_name=models.CharField(max_length=100)
     phone_no=models.CharField(max_length=10)
     address=models.CharField(max_length=200)
-    total_rooms=models.IntegerField()
+    # total_rooms=models.IntegerField()
 
     def __str__(self):
         return self.owner_name
 
-class model_form_shared(models.Model):
+class Room(models.Model):
+    owner=models.ForeignKey(owner_details,on_delete=models.CASCADE,blank=False)
     phone_no=models.CharField(max_length=10)
-    no_shared_rooms=models.IntegerField()
-    shared_room_price = models.IntegerField()
+    no_of_rooms=models.IntegerField()
+    room_price = models.IntegerField()
+    single = models.BooleanField(default=False)
     electric_bill=models.IntegerField()
     bathroom=models.CharField(choices=bth,max_length=100)
+    no_of_bathrooms = models.IntegerField()
     shared_room_pictures=models.FileField()
-
+    slug = models.SlugField(max_length=200, unique=True,default=None)
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created_on']
     def __str__(self):
         return self.phone_no
     
-class model_form_single(models.Model):
-    phone_no=models.CharField(max_length=10)
-    no_single_rooms=models.IntegerField()
-    single_room_price = models.IntegerField()
-    electric_bill=models.IntegerField()
-    bathroom=models.CharField(choices=bth,max_length=100)
-    single_room_pictures=models.FileField()
+# class model_form_single(models.Model):
+#     phone_no=models.CharField(max_length=10)
+#     no_single_rooms=models.IntegerField()
+#     single_room_price = models.IntegerField()
+#     electric_bill=models.IntegerField()
+#     bathroom=models.CharField(choices=bth,max_length=100)
+#     single_room_pictures=models.FileField()
 
-    def __str__(self):
-        return self.phone_no
+#     def __str__(self):
+#         return self.phone_no
