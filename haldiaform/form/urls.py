1489997902b24bd741f47946869686d99f03849f
unknown <amandatta02@gmail.com>
2023-07-19 02:03:21 +0530

    Owner and network form added

--- haldiaform/form/urls.py
+++ haldiaform/form/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('signup', views.signup, name="Signup" ),
     path('login/', auth_views.LoginView.as_view(template_name='signin.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('network', views.network_reg, name='network_registration'),
+    path('owner', views.owner_reg, name='owner_registration'),
     # path('signin', views.signin, name="Signin" ),
 ]
