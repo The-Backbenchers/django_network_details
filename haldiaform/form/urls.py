1fde9e9c35627d35b3460fca0d27cc81130ca467
Sayel Munsi <sayelmunsi@gmail.com>
2023-07-19 02:54:12 +0530

    views ready

--- haldiaform/form/urls.py
+++ haldiaform/form/urls.py
@@ -4,7 +4,8 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', views.Home, name="Home" ),
+    path('', views.RoomList.as_view(), name="Home" ),
+    path('<slug:slug>/', views.RoomDetail.as_view(), name='room_detail'),
     path('signup', views.signup, name="Signup" ),
     path('login/', auth_views.LoginView.as_view(template_name='signin.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
